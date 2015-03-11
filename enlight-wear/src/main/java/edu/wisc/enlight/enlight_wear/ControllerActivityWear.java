2b733d76727d819cbca4da269ee67c08dac9afaf
sschwebach <sschwebach@wisc.edu>
2015-03-10 19:14:19 -0500

    Changed up wear app

--- enlight-wear/src/main/java/edu/wisc/enlight/enlight_wear/ControllerActivityWear.java
+++ enlight-wear/src/main/java/edu/wisc/enlight/enlight_wear/ControllerActivityWear.java
@@ -1,16 +1,21 @@
 package edu.wisc.enlight.enlight_wear;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.List;
 
 
 public class ControllerActivityWear extends Activity {
-
-    FountainViewWear mView;
-    FountainViewCanvasWear leftView;
-    FountainViewCanvasWear rightView;
+    private static final int SPEECH_REQUEST_CODE = 0;
+    Button voiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +31,41 @@ public class ControllerActivityWear extends Activity {
     }
 
     public void doSetup(WatchViewStub stub){
-        addCanvas(stub);
+        voiceButton = (Button) stub.findViewById(R.id.button_voice);
+        voiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displaySpeechRecognizer();
+            }
+        });
+
     }
 
-    /**
-     * Magic voodoo code to make our fountain view less crappy to work with. Don't change the order of this please
-     */
-    private void addCanvas(WatchViewStub stub){
-        mView = new FountainViewWear(this, this);
-        leftView = new FountainViewCanvasWear(this, true, mView);
-        rightView = new FountainViewCanvasWear(this, false, mView);
-        mView.setViews(leftView, rightView);
-        LinearLayout fountainCanvasLeft = (LinearLayout) stub.findViewById(R.id.layout_canvas_left);
-        LinearLayout fountainCanvasRight = (LinearLayout) stub.findViewById(R.id.layout_canvas_right);
-        fountainCanvasLeft.addView(leftView);
-        fountainCanvasRight.addView(rightView);
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+// Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
     }
+
+
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            // Do something with spokenText
+            Toast toast = Toast.makeText(this, spokenText, Toast.LENGTH_LONG);
+            toast.show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 }
